a28b3445c7ce98e984af19fef3626c0c4249b97f
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-25 18:13:10 +0000

    savepoint #1

--- js/script.js
+++ js/script.js
@@ -5,21 +5,27 @@
 // This file contains the JS functions for index.html
 
 /**
- * This function checks if thge user has selected the random number from above
+ * This function checks if the user has selected the random number from above
  */
 
 function myButtonClicked() {
   // input
-  const userNumber = parseInt(document.getElementById("age-input").value)
+  const userAge = parseInt(document.getElementById("age-input").value)
 
-  //process
-  if (userNumber == randomNumber) {
-    // output
-    document.getElementById("age-answer").innerHTML = "You have guessed the correct number"
+  //process and output
+  if (userAge >= 17) {
+    document.getElementById("age-answer").innerHTML = "You can watch an R rated movie alone"
   }
 
-  if (userNumber != randomNumber) {
-    // output
-    document.getElementById("age-answer").innerHTML = "You have guessed the wrong number"
+  else if (userAge >= 13) {
+    document.getElementById("age-answer").innerHTML = "You can watch a PG-13 rated movie alone"
+  }
+  
+  else if (userAge >= 12) {
+    document.getElementById("age-answer").innerHTML = "You can watch a G or PG rated movie alone"
+  }
+
+  else {
+    document.getElementById("age-answer").innerHTML = "You're too young for most things"
   }
 }
